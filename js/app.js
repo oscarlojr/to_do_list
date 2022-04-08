a345e1429426613322963649e467fc3d19782d3b
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2022-04-07 21:52:59 -0300

    Iniciando variáreis JavaScript

---
+++ js/app.js
@@ -0,0 +1,5 @@
+var enterButton = document.getElementById("enter");
+var input = document.getElementById("tarefas");
+var ul = document.querySelector("ul");
+var item = document.getElementsByTagName("li");
+
