57ddd020dc9c1da3d6051d405fc0fc077986be88
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2022-04-08 20:12:54 -0300

    Adiciona itens na lista

--- js/app.js
+++ js/app.js
@@ -3,3 +3,18 @@ var input = document.getElementById("tarefas");
 var ul = document.querySelector("ul");
 var item = document.getElementsByTagName("li");
 
+function inputLength(){
+	return input.value.length;
+} 
+
+function listLength(){
+	return item.length;
+}
+
+function createListElement() { 
+	var li = document.createElement("li"); 
+	li.appendChild(document.createTextNode(input.value)); 
+	ul.appendChild(li); 
+	input.value = ""; 
+}
+
